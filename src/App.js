49df60c0a46a478e5d0e1597aebd88e82f37bd0b
Arnob-sen <arnobsen16.2@gmail.com>
2023-09-06 15:09:47 +0600

    quote_box_function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-
+import React from "react";
+import { useState } from "react";
+import QuoteBox from "./QuoteBox";
+  
 
 function App() {
   return (
     <div className="App">
-      
+      <QuoteBox/>
     </div>
   );
 }
